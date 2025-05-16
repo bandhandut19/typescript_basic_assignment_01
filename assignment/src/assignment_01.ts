38c0f52e83c293b387aa79fd851ef1c23e6eddb0
bandhandut19 <bondon21081@gmail.com>
2025-05-16 15:19:24 +0600

    problem 4 done

--- assignment/src/assignment_01.ts
+++ assignment/src/assignment_01.ts
@@ -36,3 +36,33 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 const problem_3 = concatenateArrays(["a", "b"], ["c"]);
 
 console.log(problem_3);
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  getInfo() {
+    console.log(`Make: ${this.make}, Year: ${this.year}`);
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel() {
+    console.log(`Model: ${this.model}`);
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Allion");
+myCar.getInfo();
+myCar.getModel();
